2b143cfcc35789e0138503f83fee11690a237a74
Spiff Azeta <35563797+spiffaz@users.noreply.github.com>
2022-10-15 21:36:00 +0100

    Create config.sh

---
+++ CI-CD/2/config.sh
@@ -0,0 +1,4 @@
+  #!/bin/bash
+  sudo useradd devopsuser -G wheel #Create a user and add to the wheel group. (The wheel group allows users use the sudo command, sudoers)
+  cd /etc/ssh
+  sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' sshd_config #This command searches for all occurences of string 'a' and replaces with 'b'
