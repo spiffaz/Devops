9086c4714edf7fd73f207e94aef760ed5c75c3d4
Spiff Azeta <35563797+spiffaz@users.noreply.github.com>
2022-11-02 07:25:56 +0100

    Update config.sh

--- CI-CD/2/config.sh
+++ CI-CD/2/config.sh
@@ -2,3 +2,4 @@
   sudo useradd devopsuser -G wheel #Create a user and add to the wheel group. (The wheel group allows users use the sudo command, sudoers)
   cd /etc/ssh
   sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' sshd_config #This command searches for all occurences of string 'a' and replaces with 'b'
+  service sshd restart
